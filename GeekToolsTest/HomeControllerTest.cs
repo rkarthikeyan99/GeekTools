72515d163b6b3e256a4c800cfb56ef099beb2b4f
rkarthikeyan99 <r_karthikeyan2003@yahoo.com>
2015-09-26 18:29:34 +0530

    Adding Test Project

---
+++ GeekToolsTest/HomeControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GeekToolsTest
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void PositiveTest()
+        {
+            int x = 7;
+            int y = 7;
+            Assert.AreEqual(x, y);
+        }
+    }
+}
